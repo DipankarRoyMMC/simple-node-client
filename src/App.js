43b006017b8ab9706af7d14a0806792a08907319
DIPANKAR <you@example.com>
2023-01-22 11:44:39 +0600

    simple-node-client

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/users')
+      .then(res => res.json())
+      .then(data => setUsers(data));
+
+  }, []);
+
+  const handleAddUser = event => {
+    event.preventDefault();
+    console.log('Form submitting!!');
+
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const user = { name, email };
+
+    fetch('http://localhost:5000/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    })
+      .then(res => res.json())
+      .then(data => {
+        const newUsers = [...users, data];
+        setUsers(newUsers);
+
+        console.log(data);
+      })
+
+
+      .catch((error) => {
+        console.error('Error', error);
+      });
+
+    event.target.reset();
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome Simple Node Client</h1>
+
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" placeholder='Enter your name' />
+        <br></br>
+        <input type="email" name="email" placeholder='Enter email address' />
+        <br></br>
+        <input type="submit" value="Submit" />
+      </form>
+
+      <h2>Users: {users.length}</h2>
+      {
+        users.map(user => <li key={user._id}>{user.name}, {user.email}</li>)
+      }
+
+
     </div>
   );
 }
